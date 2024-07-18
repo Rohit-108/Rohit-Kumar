f9a08e161509c5f252e8409d64ac6f4fb10df86b
Rohit-108 <rohitsharma1082001@gmail.com>
2024-07-18 21:41:35 +0530

    commit

--- src/components/ArchiveCard.jsx
+++ src/components/ArchiveCard.jsx
@@ -1,31 +1,61 @@
 import {RxOpenInNewWindow} from "react-icons/rx";
+import { TbBrandGithub } from "react-icons/tb";
 import { FaRegFolder } from "react-icons/fa";
+import {motion} from 'framer-motion';
 
 
 
 
-const ArchiveCard = ({title,des,listitem, link}) => {
+const ArchiveCard = ({title,des,listItem, link}) => {
   return (
-    <a href={link} target="_blank">
-      {" "}
-      <div className="w-full h-80 rounded-lg bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 transition-transform duration-300 group">
-        <div className="flex justify-between items-center">
-          <FaRegFolder className="text-4xl text-textGreen" />
-          <RxOpenInNewWindow className="text-4xl hover:text-textGreen" />
-        </div>
-        <div>
-          <h2 className="text-xl font-titleFont font-semibold tracking-wide group-hover:text-textGreen ">{title}</h2>
-          <p className="text-sm mt-3">{des}</p>
-        </div>
-        <ul className="text-xs mdl:text-sm text-textDark flex items-center gap-2 justify-between flex-wrap">
-          {
-            listitem.map((item,i) => (
-                <li key={i}>{item}</li>
-            ))
-          }
+    <motion.a 
+    initial={{ opacity: 0 }}
+      transition={{ delay: 0.2, duration: 0.5 }}
+      animate={{ opacity: 1 }}
+    href={link} target="_blank" className="relative transition-all duration-200 ease-in-out">
+      <div className="w-full h-ful rounded-lg  bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 duration-200 transition-transform    group   items-start relative  px-7 py-8     ease-in-out overflow-auto shadow-aboutShadow cursor-pointer">
+      <header>
+          <div className="flex justify-between items-center mb-[35px]">
+            <div className="text-themeBlue text-5xl">
+              <FaRegFolder />
+            </div>
+            <div className="flex items-center -mr-[10px] ">
+              <a
+                href={link}
+                className="flex justify-center items-center px-[7px] py-[5px] relative z-[1] text-2xl text-secondaryText transition-all duration-200  hover:text-themeBlue"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TbBrandGithub />
+              </a>
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center px-[7px] py-[5px] relative z-[1] text-2xl text-secondaryText transition-all duration-200  hover:text-themeBlue"
+              >
+                <RxOpenInNewWindow/>
+              </a>
+            </div>
+          </div>
+
+          <h3 className="mb-[10px] text-primaryText transition-all duration-500  group-hover:text-themeBlue text-xl font-bold leading-[1.1] font-fontHeading">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <div className="block absolute z-0 w-full h-full top-0 left-0"></div>
+              {title}
+            </a>
+          </h3>
+          <div className="text-secondaryText text-[14px] font-semibold font-fontParagraph">
+            <p>{des}</p>
+          </div>
+        </header>
+        <ul className="text-xs mdl:text-sm text-textDark flex items-end list-none gap-2 justify-between flex-wrap mt-[20px]">
+          {listItem.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
         </ul>
       </div>
-    </a>
+    </motion.a>
   );
 }
 
--- src/components/OtherProjects.jsx
+++ src/components/OtherProjects.jsx
@@ -1,17 +1,21 @@
 import {motion} from "framer-motion";
 import {ArchiveCard} from "../components"
+import PropTypes from "prop-types";
+import React, { useState } from 'react';
 
 
 const OtherProjects = () => {
+    const [showMore, setShowMore] =useState(false)
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      transition={{ delay: 0.2, duration: 0.5 }}
-      animate={{ opacity: 1 }}
-      className="max-w-container mx-auto px-4 py-24 relative transition-all duration-200 ease-in-out"
+    <motion.section
+      initial={{ opacity: 0, y: 25 }}
+        transition={{ delay: 0.5, duration: 1 }}
+        viewport={{ once: true }}
+        whileInView={{ opacity: 1, y: 0 }}
+      className="max-w-container mx-auto px-4 py-24 relative transition-all duration-200 ease-in-out flex flex-col items-center py-[60px] px-0 sm:py-[80px] sm:px-0 my-0 mx-auto "
     >
       <div className="w-full flex flex-col items-center ">
-        <h2 className="text-3xl font-titleFont font-semibold">
+        <h2 className="text-sectionHeading m-0 mb-[10px] font-bold text-primaryText leading-[1.1]">
           Other Noteworthy Projects
         </h2>
         <p className="text-sm font-titleFont text-textGreen">
@@ -25,9 +29,162 @@ const OtherProjects = () => {
           listItem={["HTML", "CSS", "JavaScript"]}
           link="https://github.com/Rohit-108/password-generator"
         />
+        <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        {
+            showMore && (
+                <>
+                <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.1 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.2 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.4 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.5 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.6 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.7 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.8 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        <motion.div 
+                intial={{ opacity: 0 }}
+                whileInview={{ opacity: 1 }}
+                transition={{ delay: 0.9 }}
+                >
+                 <ArchiveCard
+          title="Password Generator"
+          des="This is a password generator web app.It is a dynamic apporch to genrate password randomly with the diffrent constriants.This is completely responsive and created with help of javascript."
+          listItem={["HTML", "CSS", "JavaScript"]}
+          link="https://github.com/Rohit-108/password-generator"
+        />
+        </motion.div>
+        </>
+
+
+            )
+        }
       </div>
-    </motion.div>
+      <div className=" flex items-center justify-center">
+        <button 
+        onClick={() => setShowMore(true)}
+        className="font-mono leading-4 text-themeBlue bg-transparent border border-themeBlue rounded-[4px] px-7 py-5 mx-auto mt-[80px] text-[14px] transition-all ease-in-out duration-300 hover:shadow-buttonShadow hover:-translate-x-1 hover:-translate-y-1">Show More </button>
+      </div>
+    </motion.section>
   );
-}
+};
+
+
 
 export default OtherProjects
