eb34b62f07e1ac08a4d10bb2599630407bedca4e
Rohit Kumar <rohitsharma1082001@gmail.com>
2025-07-10 00:19:53 +0530

    add experience

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -2,7 +2,29 @@
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <div>
+            <div className="flex items-center justify-start relative mt-[10px] mb-[40px] mx-0 w-full whitespace-nowrap text-sectionHeading text-primaryText font-bold">
+                <span className="relative -bottom-1 text-themeBlue font-mono text-smallSize font-semibold mr-[10px]">
+                    03.
+                </span>
+                <h2 className="text-sectionHeading">Experience</h2>
+                <div className="block sm:hidden w-full h-[1px] bg-blue-400/20 relative top-[5px] ml-[10px] "></div>
+                <div className="hidden sm:block w-full md:w-[200px] lg:w-[300px] h-[1px] bg-blue-400/20 relative top-[5px] ml-[20px]"></div>
+            </div>
+            <div>Cybersecurity Analyst  <span className="text-themeBlue">@Wipro</span></div>
+            <div>Mar 2025 - Present</div>
+            <div>Pune, IND</div>
+            <div>
+                <p>As a dedicated Cybersecurity Analyst at Wipro, I specialize in Identity and Access Management (IDAM), ensuring robust security protocols and practices across digital identities within organizational frameworks.</p>
+                <p>Practiced and implemented core IDAM principles including provisioning, de-provisioning, authentication, authorization, and identity lifecycle management.</p>
+                <p>Worked with industry-standard tools like SailPoint to enhance identity governance and automate access certification processes.</p>
+                <p>Developed understanding of key cybersecurity concepts including risk management, vulnerability assessment, and threat detection.</p>
+                <p>Onboarded applications to SailPoint, automated access reviews, and improved compliance with regulatory standards. Built secure JML workflows, integrated SSO/MFA, and strengthened zero-trust security. </p>
+                <p>Recognized for precision in access control design, strong cross-functional collaboration, attention to security compliance, and consistent delivery of secure and scalable identity solutions.</p>
+            </div>
+        </div>
+
+
     )
 }
 
