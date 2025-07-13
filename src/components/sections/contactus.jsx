3c8420ce9368a76dc00d8e335ff2f5c2d05eafc1
Rohit Kumar <rohitsharma1082001@gmail.com>
2025-07-13 23:54:23 +0530

    add contact section:

--- src/components/sections/contactus.jsx
+++ src/components/sections/contactus.jsx
@@ -6,6 +6,10 @@ const contactus = () => {
             <div>contactus</div>
             <div>rohitsharma1082001@gmail.com</div>
             <div>+91 9027748842</div>
+            <div className="text-themeBlue">LinkedIn: <a href="https://www.linkedin.com/in/rohit-sharma-1082001/" target="_blank" rel="noopener noreferrer">Rohit Sharma</a></div>
+            <div className="text-themeBlue">GitHub: <a href="https://   github.com/rohitsharma1082001" target="_blank" rel="noopener noreferrer">rohitsharma1082001</a></div>
+            <div className="text-themeBlue">Portfolio: <a href="https://rohitsharma1082001.github.io/" target="_blank" rel="noopener noreferrer">rohitsharma1082001.github.io</a></div>
+            <div className="text-themeBlue">Resume: <a href="https://rohitsharma1082001.github.io/resume.pdf" target="_blank" rel="noopener noreferrer">Download Resume</a></div>
             <div></div>
         </div>
 
