13a40cf43aa32771d0ab7db0310bf35d8a723290
Rohit Kumar <rohitsharma1082001@gmail.com>
2025-07-13 23:51:33 +0530

    contact section add

---
+++ src/components/sections/contactus.jsx
@@ -0,0 +1,15 @@
+
+
+const contactus = () => {
+    return (
+        <div>
+            <div>contactus</div>
+            <div>rohitsharma1082001@gmail.com</div>
+            <div>+91 9027748842</div>
+            <div></div>
+        </div>
+
+    )
+}
+
+export default contactus
